95ca1ed871287e00b47b326b8bea33667c4806b0
kar <hi@kar.gs>
2020-01-25 16:49:43 +0100

    new UI first attempt PoC

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import icon from './ico-blokada.svg';
+import iconAdd from './ico-add.svg';
+import iconLock from './ico-3.svg';
+import iconPerson from './ico-4.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          <Col className="fixedWidth">
+            <Container>
+              <Row>
+                <Col className="col-center">
+                <img src={icon} className="App-logo" alt="" />
+                </Col>
+              </Row>
+            </Container>
+            <Container>
+              <Row>
+                <Col className="col-center">
+                  <img src={iconAdd} className="icon" alt="" />
+                </Col>
+              </Row>
+              <Row>
+                <Col className="col-center">
+                  <img src={iconLock} className="icon" alt="" />
+                </Col>
+              </Row>
+              <Row>
+                <Col className="col-center">
+                  <img src={iconPerson} className="icon" alt="" />
+                </Col>
+              </Row>
+            </Container>
+          </Col>
+          <Col>
+            <p>Content testowy<br/><br/><br/><br/><br/><br/></p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
